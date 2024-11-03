78fc01ec113eb65f1fc4cb696d7da1047645945a
VuBaTai2004 <22521289@gm.uit.edu.vn>
2024-11-03 14:14:16 +0700

    small update on AdminClassModifyFragment

--- app/src/main/java/com/example/chilltime/AdminClassModifyFragment.java
+++ app/src/main/java/com/example/chilltime/AdminClassModifyFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -24,18 +25,22 @@ public class AdminClassModifyFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_admin_class_modify, container, false);
         Button btn = view.findViewById(R.id.admin_btn_modify_class);
         ImageView iv = view.findViewById(R.id.admin_iv_back_modify);
+        EditText et_id = view.findViewById(R.id.admin_et_class_id);
+        EditText et_subject = view.findViewById(R.id.admin_et_class_subject);
+
+        et_id.setText(adminClass.getClassId().toString());
+        et_subject.setText(adminClass.getClassSubject().toString());
+
         iv.setOnClickListener(view1 -> {
             FragmentManager fragmentManager = ((AppCompatActivity) view1.getContext()).getSupportFragmentManager();
             FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
             fragmentTransaction.replace(R.id.fragment_container, new AdminClassFragment());
-            fragmentTransaction.addToBackStack(null); // Optional: adds the transaction to the back stack
             fragmentTransaction.commit();
         });
         btn.setOnClickListener(view2 -> {
             FragmentManager fragmentManager = ((AppCompatActivity) view2.getContext()).getSupportFragmentManager();
             FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
             fragmentTransaction.replace(R.id.fragment_container, new AdminClassFragment());
-            fragmentTransaction.addToBackStack(null); // Optional: adds the transaction to the back stack
             fragmentTransaction.commit();
         });
 

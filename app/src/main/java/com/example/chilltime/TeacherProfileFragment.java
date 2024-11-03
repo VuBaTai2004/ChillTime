89c80eb54c54f67f05f81a957ed31992038669f8
nauqpm <134082737+nauqpm@users.noreply.github.com>
2024-11-03 21:38:58 +0700

    add the show password feature

--- app/src/main/java/com/example/chilltime/TeacherProfileFragment.java
+++ app/src/main/java/com/example/chilltime/TeacherProfileFragment.java
@@ -1,12 +1,17 @@
 package com.example.chilltime;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -14,14 +19,34 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
 
 public class TeacherProfileFragment extends Fragment {
+    private boolean isPasswordVisible = false;
+    private TeacherProfile teacherProfile;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_teacher_profile, container, false);
 
+        teacherProfile = new TeacherProfile("Pham Minh E", "nauqpm", "04052004", "0868480060", "quanpham0405@gmail.com");
 
+        TextView tv_name = view.findViewById(R.id.text_name);
+        TextView tv_user = view.findViewById(R.id.text_username);
+        TextView tv_password = view.findViewById(R.id.text_password);
+        TextView tv_phone = view.findViewById(R.id.text_phone);
+        TextView tv_email = view.findViewById(R.id.text_email);
+
+        tv_name.setText(teacherProfile.getName());
+        tv_user.setText(teacherProfile.getUsername());
+        tv_phone.setText(teacherProfile.getPhone());
+        tv_email.setText(teacherProfile.getEmail());
+
+        ImageView imageView = view.findViewById(R.id.profile_image);
+        imageView.setImageResource(R.drawable.icon_teacher1);
+
+        ImageView btnViewPassword = view.findViewById(R.id.btn_view_password);
         Button btn = view.findViewById(R.id.teacher_btn_logout);
+
+        setPasswordText(tv_password, isPasswordVisible);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,6 +56,31 @@ public class TeacherProfileFragment extends Fragment {
                 startActivity(intent);
             }
         });
+
+
+        btnViewPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isPasswordVisible = !isPasswordVisible;
+                setPasswordText(tv_password, isPasswordVisible);
+            }
+        });
+
         return view;
+    }
+
+    private void setPasswordText(TextView textView, boolean isVisible) {
+        SpannableString spannableString;
+        String password = teacherProfile.getPassword();
+
+        if (isVisible) {
+            spannableString = new SpannableString(password);
+            spannableString.setSpan(new ForegroundColorSpan(Color.BLACK), 0, password.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        } else {
+            String hiddenPassword = new String(new char[password.length()]).replace("\0", "*");
+            spannableString = new SpannableString(hiddenPassword);
+            spannableString.setSpan(new ForegroundColorSpan(Color.GRAY), 0, hiddenPassword.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        }
+        textView.setText(spannableString);
     }
 }

17454db66c6769fc6df68000087bf411d96f7dcd
nauqpm <134082737+nauqpm@users.noreply.github.com>
2024-11-05 13:20:04 +0700

    delete view password feature in teacher

--- app/src/main/java/com/example/chilltime/TeacherProfileFragment.java
+++ app/src/main/java/com/example/chilltime/TeacherProfileFragment.java
@@ -19,7 +19,6 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
 
 public class TeacherProfileFragment extends Fragment {
-    private boolean isPasswordVisible = false;
     private TeacherProfile teacherProfile;
 
     @Override
@@ -27,11 +26,10 @@ public class TeacherProfileFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_teacher_profile, container, false);
 
-        teacherProfile = new TeacherProfile("Pham Minh E", "nauqpm", "04052004", "0868480060", "quanpham0405@gmail.com");
+        teacherProfile = new TeacherProfile("Pham Minh E", "nauqpm", "0868480060", "quanpham0405@gmail.com");
 
         TextView tv_name = view.findViewById(R.id.text_name);
         TextView tv_user = view.findViewById(R.id.text_username);
-        TextView tv_password = view.findViewById(R.id.text_password);
         TextView tv_phone = view.findViewById(R.id.text_phone);
         TextView tv_email = view.findViewById(R.id.text_email);
 
@@ -43,10 +41,8 @@ public class TeacherProfileFragment extends Fragment {
         ImageView imageView = view.findViewById(R.id.profile_image);
         imageView.setImageResource(R.drawable.icon_teacher1);
 
-        ImageView btnViewPassword = view.findViewById(R.id.btn_view_password);
         Button btn = view.findViewById(R.id.teacher_btn_logout);
 
-        setPasswordText(tv_password, isPasswordVisible);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,30 +53,7 @@ public class TeacherProfileFragment extends Fragment {
             }
         });
 
-
-        btnViewPassword.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                isPasswordVisible = !isPasswordVisible;
-                setPasswordText(tv_password, isPasswordVisible);
-            }
-        });
-
         return view;
     }
 
-    private void setPasswordText(TextView textView, boolean isVisible) {
-        SpannableString spannableString;
-        String password = teacherProfile.getPassword();
-
-        if (isVisible) {
-            spannableString = new SpannableString(password);
-            spannableString.setSpan(new ForegroundColorSpan(Color.BLACK), 0, password.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-        } else {
-            String hiddenPassword = new String(new char[password.length()]).replace("\0", "*");
-            spannableString = new SpannableString(hiddenPassword);
-            spannableString.setSpan(new ForegroundColorSpan(Color.GRAY), 0, hiddenPassword.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-        }
-        textView.setText(spannableString);
-    }
 }

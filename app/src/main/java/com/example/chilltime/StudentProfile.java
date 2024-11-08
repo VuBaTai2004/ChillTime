27ca0f9b26998eec58235a28830c4333d4590d1e
VuBaTai2004 <22521289@gm.uit.edu.vn>
2024-11-08 10:54:31 +0700

    Update

---
+++ app/src/main/java/com/example/chilltime/StudentProfile.java
@@ -0,0 +1,28 @@
+package com.example.chilltime;
+
+public class StudentProfile {
+    private String name;
+    private String username;
+    private String phone;
+    private String email;
+
+    public StudentProfile (String name, String username, String phone, String email) {
+        this.name = name;
+        this.username = username;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public String getEmail(){
+        return email;
+    }
+}
--- app/src/main/java/com/example/chilltime/StudentProfileFragment.java
+++ app/src/main/java/com/example/chilltime/StudentProfileFragment.java
@@ -9,6 +9,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class StudentProfileFragment extends Fragment {
     @Override
@@ -16,6 +18,23 @@ public class StudentProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View itemView = inflater.inflate(R.layout.fragment_student_profile, container, false);
+
+        StudentProfile studentProfile = new StudentProfile("Vu Ba F", "vubatai", "02131832", "abcdxyz@gmail.com");
+
+        TextView tv_name = itemView.findViewById(R.id.text_name);
+        TextView tv_user = itemView.findViewById(R.id.text_username);
+        TextView tv_phone = itemView.findViewById(R.id.text_phone);
+        TextView tv_email = itemView.findViewById(R.id.text_email);
+
+        tv_name.setText(studentProfile.getName());
+        tv_user.setText(studentProfile.getUsername());
+        tv_phone.setText(studentProfile.getPhone());
+        tv_email.setText(studentProfile.getEmail());
+
+        ImageView imageView = itemView.findViewById(R.id.profile_image);
+        imageView.setImageResource(R.drawable.icon_student1);
+
+
         Button btn = itemView.findViewById(R.id.student_btn_logout);
         btn.setOnClickListener(view -> {
             Intent intent = new Intent(getActivity(), MainActivity.class);

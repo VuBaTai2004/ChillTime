a7d6b8ecbbf3ef1923c12d770b28c8d56b1469f5
VuBaTai2004 <22521289@gm.uit.edu.vn>
2024-11-08 08:30:27 +0700

    Change StudentProfileFragment - add logout button feature

--- app/src/main/java/com/example/chilltime/StudentProfileFragment.java
+++ app/src/main/java/com/example/chilltime/StudentProfileFragment.java
@@ -1,5 +1,6 @@
 package com.example.chilltime;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,12 +8,21 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class StudentProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_student_profile, container, false);
+        View itemView = inflater.inflate(R.layout.fragment_student_profile, container, false);
+        Button btn = itemView.findViewById(R.id.student_btn_logout);
+        btn.setOnClickListener(view -> {
+            Intent intent = new Intent(getActivity(), MainActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            startActivity(intent);
+        });
+
+        return itemView;
     }
 }

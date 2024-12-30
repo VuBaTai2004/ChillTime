1ad52d712c71399704d8f1934277e3a446702515
VuBaTai2004 <22521289@gm.uit.edu.vn>
2024-12-31 00:49:58 +0700

    not done for this sheesh

--- app/src/main/java/com/example/chilltime/AdminClass.java
+++ app/src/main/java/com/example/chilltime/AdminClass.java
@@ -1,15 +1,21 @@
 package com.example.chilltime;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
 import java.util.ArrayList;
 
 public class AdminClass extends AppCompatActivity {
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +31,25 @@ public class AdminClass extends AppCompatActivity {
 
         classes.add(new TeacherClass("NT131.P13", "Hệ thống Nhúng mạng không dây", "10", "Nguyễn Văn A"));
         classes.add(new TeacherClass("NT131.P14", "Hệ thống Nhúng mạng không dây", "10", "Nguyễn Văn A"));
+
+        Intent intent = this.getIntent();
+        String subject = intent.getStringExtra("classId");
+        db.collection("courses").document(subject).collection("course_list")
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        classes.clear();
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            TeacherClass student = new TeacherClass(document.getString("id"), document.getString("subject"),
+                                    document.getString("numberStudent").toString(), document.getString("teacher"));
+                            Log.d("test", document.getData().toString());
+                            classes.add(student);
+                        }
+                        adapter.notifyDataSetChanged();
+                    } else {
+                        Log.w("err", "Error getting documents.", task.getException());
+                    }
+                });
 
         adapter.notifyDataSetChanged();
 

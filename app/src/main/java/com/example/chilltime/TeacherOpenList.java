0cb97f5b149d2b98f5c8fdd2ef91352788858d83
VuBaTai2004 <22521289@gm.uit.edu.vn>
2024-12-31 17:00:56 +0700

    AdminOpenClass - get data and display done

--- app/src/main/java/com/example/chilltime/TeacherOpenList.java
+++ app/src/main/java/com/example/chilltime/TeacherOpenList.java
@@ -71,13 +71,14 @@ public class TeacherOpenList extends AppCompatActivity {
                                         @Override
                                         public void onComplete(@NonNull Task<QuerySnapshot> task) {
                                             if (task.isSuccessful()) {
-                                                String name, phone, email;
+                                                String name, phone, email, id;
                                                 for (QueryDocumentSnapshot document1 : task.getResult()) {
+                                                    id = document1.getString("id");
                                                     name = document1.getString("name");
                                                     phone = document1.getString("phone");
                                                     email = document1.getString("email");
-                                                    TeacherProfile student = new TeacherProfile(name, phone,
-                                                            email, timestamp);
+                                                    TeacherProfile student = new TeacherProfile(name, id, phone,
+                                                            email);
                                                     students.add(student);
                                                     Log.d("test", document1.getData().toString());
                                                     adapter.notifyDataSetChanged();
